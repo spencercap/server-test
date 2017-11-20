7ed5e7f679a4b600ec6dd024c55ba572563d7174
spencercap <cappiello.spencer@gmail.com>
2017-11-20 13:10:54 -0500

    works w 2 serers!

--- server2.js
+++ server2.js
@@ -1,14 +1,14 @@
 
-var port = 50251;
+//var port = 49102;
 // var port = 3011;
 
 var ioc = require( 'socket.io-client' );
-var client = ioc.connect( "https://serverrr.herokuapp.com/:" + port );
+var client = ioc.connect( "https://serverrr.herokuapp.com"); // don't need a port for heroku!! / online servers 
 // var client = ioc.connect( "http://localhost:" + port );
 
 
 client.once( "connect", function () {
-    console.log( 'Client: Connected to port ' + port );
+    // console.log( 'Client: Connected to port ' + port );
 
     client.emit( "echo", "Hello World", function ( message ) {
         console.log( 'Echo received: ', message );
